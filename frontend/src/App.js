c9898773148ae0cd2d3cecc48ca44d748963d3fe
ndr911 <44654040+ndr911@users.noreply.github.com>
2023-07-30 12:34:33 -0400

    Set up compenents and BS template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+    <Header/>
       <h1>Welcome to My Store</h1>
+      <Footer />
     </>
   );
 }
