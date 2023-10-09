4f1ceb3e9b0caf33baa1a7f9d42b4c61a1e9f7b5
ndr911 <44654040+ndr911@users.noreply.github.com>
2023-10-09 11:40:12 -0400

    redux changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,37 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Col, Row} from 'react-bootstrap'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
 
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const {loading, products, error} = productList
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-    setProducts(data)
-    }
-
-    fetchProducts()
-  })
-
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
+      {
+        loading ? (<Loader />) 
+          : error ? (<Message variant='danger'>{error}</Message>) : (<Row>
         {products.map(p => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={p} />
           </Col>
         ))
 
         }
-      </Row>
+      </Row>)
+      }
+      
     </>
   )
 }
---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {productListReducer} from './reducers/productReducers'
+
+
+const rootReducer = combineReducers({
+    productList: productListReducer
+})
+const store = configureStore (
+    {
+        reducer: rootReducer,
+        preloadedState: {}
+
+    }
+)
+
+export default store
